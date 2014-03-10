1ec220d189cf73fba70ab25e315b049f7ddb0140
Joel Bradshaw <cincodenada@gmail.com>
2014-03-09 18:59:53 -0700

    First work on unified timeline

--- mapthing/static/js/mapthing.js
+++ mapthing/static/js/mapthing.js
@@ -1,6 +1,7 @@
 var seglist = {};
+var uni_list, uni_missing, uni_interp;
 var map;
-var canvas, dc;
+var selview, dc;
 $(function() {
     /*
     tm = TimeMap.init({
@@ -12,10 +13,10 @@ $(function() {
     */
     map = new mxn.Mapstraction('map', 'googlev3')
 
-    canvas = document.getElementById('sel_view').firstElementChild;
-    canvas.width = $(canvas).innerWidth();
-    canvas.height = $(canvas).innerHeight();
-    dc = canvas.getContext('2d');
+    selview = document.getElementById('sel_view').firstElementChild;
+    selview.width = $(selview).innerWidth();
+    selview.height = $(selview).innerHeight();
+    dc = selview.getContext('2d');
 
     map.enableScrollWheelZoom();
     map.addControls({
@@ -44,20 +45,24 @@ $(function() {
 
     $('#seg_list').on('mouseover','li a',function() {
         segid = $(this).data('segid');
-        $.each(seglist[segid], function(idx, seg) {
-            seg.oldcolor = seg.color;
-            seg.setColor('#0000FF');
-            seg.setWidth('6');
-            seg.update();
-        })
+        if(seglist[segid]) {
+            $.each(seglist[segid], function(idx, seg) {
+                seg.oldcolor = seg.color;
+                seg.setColor('#0000FF');
+                seg.setWidth('6');
+                seg.update();
+            })
+        }
     });
     $('#seg_list').on('mouseout','li a',function() {
         segid = $(this).data('segid');
-        $.each(seglist[segid], function(idx, seg) {
-            seg.setColor(seg.oldcolor);
-            seg.setWidth('4');
-            seg.update();
-        })
+        if(seglist[segid]) {
+            $.each(seglist[segid], function(idx, seg) {
+                seg.setColor(seg.oldcolor);
+                seg.setWidth('4');
+                seg.update();
+            })
+        }
     });
     $('#controls').on('click','button',function(evt, ui) {
         doAction($(this).data('action'),$(this).data('value'));
@@ -158,7 +163,7 @@ function draw_selview(timerange) {
     }
 
     dc.fillStyle = "rgba(0,0,0,0.2)";
-    dc.clearRect(0,0,canvas.width,canvas.height);
+    dc.clearRect(0,0,selview.width,selview.height);
 
     for(idx in point_data.timepoints) {
         curtime = point_data.timepoints[idx].time/1000;
@@ -166,14 +171,48 @@ function draw_selview(timerange) {
             || (maxtime && (curtime > maxtime))
         ) { continue; }
 
-        xpos = (curtime-mintime)/(maxtime-mintime)*canvas.width;
-        dc.fillRect(xpos,0,1,canvas.height);
+        xpos = (curtime-mintime)/(maxtime-mintime)*selview.width;
+        dc.fillRect(xpos,0,1,selview.height);
+    }
+}
+
+function draw_uniview() {
+    var start, end;
+    for(var i in uni_list) { start = i; break; }
+    end = uni_list.length;
+
+    var uniview = document.getElementById('uni_view').firstElementChild;
+    uniview.width = $(uniview).innerWidth();
+    uniview.height = 1;
+    var uvdc = uniview.getContext('2d');
+
+
+    var scalefact = uniview.width/(end-start);
+    uvdc.fillStyle = "rgb(255,0,0)";
+    for(var r in uni_missing) {
+        var range = uni_missing[r];
+        uvdc.fillRect(
+            (range[0]-start)*scalefact,0,
+            (range[1]-start+1)*scalefact,1
+        );
+    }
+    uvdc.fillStyle = "rgb(255,255,0)";
+    for(var r in uni_interp) {
+        var range = uni_interp[r];
+        uvdc.fillRect(
+            (range[0]-start+1)*scalefact,0,
+            (range[1]-start-1)*scalefact,1
+        );
     }
 }
 
 function draw_mapview(timerange) {
     var points = Array();
     var curseg;
+
+    var uni_tick = $('#uni_interval').val();
+    var uni_thresh = $('#uni_interp').val();
+
     var mintime = false, maxtime = false;
     if(timerange) {
         mintime = timerange[0];
@@ -181,13 +220,61 @@ function draw_mapview(timerange) {
     }
     seglist = {};
     newpolys = [];
+    uni_list = [];
+    uni_avg = [];
+    uni_missing = [];
+    uni_interp = [];
 
+    var lastpoint = null;
+    var lasttick = null;
     for(idx in point_data.timepoints) {
         curpoint = point_data.timepoints[idx];
         curtime = curpoint.time/1000;
         if((mintime && (curtime < mintime))
             || (maxtime && (curtime > maxtime))
         ) { continue; }
+
+        curtick = Math.round(curtime/uni_tick);
+        if(lasttick && (lasttick != curtick)) {
+            //We're in a new point, check on our points
+            var lat, lon;
+            if(uni_avg.length > 1) {
+                lat = lon = 0;
+                for(i=0;i<uni_avg.length;i++) {
+                    lat += uni_avg[i].lat;
+                    lon += uni_avg[i].lon;
+                }
+                lat = lat/uni_avg.length;
+                lon = lon/uni_avg.length;
+            } else if(uni_avg.length == 1) {
+                lat = uni_avg[0].lat;
+                lon = uni_avg[0].lon;
+            }
+            
+            uni_avg = [];
+            uni_list[lasttick] = [lat, lon];
+
+            var diff = curtick - lasttick;
+            if(diff == 1) {
+                //No interpolation necessary
+                //Carry on
+            } else if(diff <= uni_thresh) {
+                //Just do some linear interpolation
+                for(var t = 1; t < diff; t++) {
+                    uni_list[lasttick + t] = [
+                        (curpoint.lat - uni_list[lasttick][0])*(t/diff),
+                        (curpoint.lon - uni_list[lasttick][1])*(t/diff)
+                    ];
+                }
+                uni_interp.push([lasttick + 1, curtick - 1]);
+            } else {
+                //Too much missing, add it to the missing list
+                uni_missing.push([lasttick + 1, curtick - 1]);
+            }
+        }
+        //In any case, throw the point on the average list
+        uni_avg.push(curpoint);
+        lasttick = curtick;
 
         if(!seglist[curpoint.segid]) {
             seglist[curpoint.segid] = {
@@ -224,4 +311,6 @@ function draw_mapview(timerange) {
             map.addPolyline(curseg.lines[subseg]);
         }
     }
+
+    draw_uniview();
 }
