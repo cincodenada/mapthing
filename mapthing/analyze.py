69369c6e276af78f850103803d9378ca5ad5356a
Joel Bradshaw <cincodenada@gmail.com>
2016-01-30 18:59:22 -0800

    Initial map rendering tests

--- mapthing/analyze.py
+++ mapthing/analyze.py
@@ -4,6 +4,8 @@ from datetime import datetime
 from sqlalchemy import engine_from_config
 from ConfigParser import ConfigParser
 
+import mapnik
+
 startdate = datetime.strptime('2015-12-01','%Y-%m-%d')
 enddate = datetime.strptime('2015-12-31','%Y-%m-%d')
 
@@ -13,9 +15,12 @@ engine = engine_from_config({
 models.DBSession.configure(bind=engine)
 models.Base.metadata.bind = engine
 
-hist = gps.History()
-for p, s, t in models.Point.getByDate(startdate, enddate):
-    print ".",
-    hist.add_point(p)
+#hist = gps.History()
+#for p, s, t in models.Point.getByDate(startdate, enddate):
+#    print ".",
+#    hist.add_point(p)
 
-print hist.get_trips()
+m = mapnik.Map(10*256,10*256)
+mapnik.load_map(m, 'mapstyle.xml')
+m.zoom_all()
+mapnik.render_to_file(m, 'test.png')
