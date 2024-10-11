63c76428630920746f0e6014667a86e3c106a1d2
Ell Bradshaw <ell@c9a.co>
2024-10-11 15:51:10 -0700

    Initial Stop template

--- mapthing/static/js/vue/src/components/Stop.vue
+++ mapthing/static/js/vue/src/components/Stop.vue
@@ -5,6 +5,14 @@ defineProps<{
 }>()
 </script>
 <template>
+  <div class="stop_time">{{stop.start_time}}</div>
+  <div class="stop_container place">
+      <a href="#" class="trip_stop">
+        <span>{{stop.name}}</span>
+      </a>
+    <span class="stop_latlon">{{stop.location.latitude}}, {{stop.location.longitude}}</span>
+  </div>
+  <div class="stop_time">{{stop.end_time}}</div>
 </template>
 <style scoped>
 </style>
