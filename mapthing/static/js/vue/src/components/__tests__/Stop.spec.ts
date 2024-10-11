a7439f85ad11577ddd629f59bd2d6b27d1157325
Ell Bradshaw <ell@c9a.co>
2024-10-11 15:46:42 -0700

    Reomve ng-if

--- mapthing/static/js/vue/src/components/__tests__/Stop.spec.ts
+++ mapthing/static/js/vue/src/components/__tests__/Stop.spec.ts
@@ -17,22 +17,9 @@ describe('Stop', () => {
     expect(wrapper).toMatchInlineSnapshot(`
       <div class="stop_time">2024/10/10 20:00</div>
       <div class="stop_container place">
-          <a ng-if="!event.isEditing" href="#" class="trip_stop">
+          <a href="#" class="trip_stop">
               <span>Home</span>
           </a>
-          <div ng-if="event.isEditing" class="trip_stop">
-              <form>
-                  <select name="type" ng-model="pendingLoc.type">
-                      <option value="region">🏘️</option>
-                      <option value="place">👁️</option>
-                      <option value="waypoint">🚏</option>
-                      <option value="ignore">🗑️</option>
-                  </select>
-                  <input type="text" ng-model="pendingLoc.name"/>
-                  <input type="submit" value="✅"/>
-                  <input type="reset" value="❌"/>
-              </form>
-          </div>
           <span class="stop_latlon">42.5, 122.5</span>
       </div>
       <div class="stop_time">2024/10/11 08:30</div>
