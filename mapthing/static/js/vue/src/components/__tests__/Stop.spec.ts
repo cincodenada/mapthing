4426494bc9f5de4bb0df0b30e65982e83adcc954
Ell Bradshaw <ell@c9a.co>
2024-10-12 15:45:01 -0700

    Format HTML for compare
    This was WAY more annoying than it should have been, npm is a disaster

--- mapthing/static/js/vue/src/components/__tests__/Stop.spec.ts
+++ mapthing/static/js/vue/src/components/__tests__/Stop.spec.ts
@@ -1,9 +1,17 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
+import xmlFormat from 'xml-formatter'
+import { promisify } from 'util'
+
 import Stop from '../Stop.vue'
 import StopModel from '@/models/Stop'
 import Locations from '@/fixtures/Location'
+
+const clean = (html) => {
+  const out = xmlFormat(`<root>${html}</root>`, { collapseContent: true, indentation: '  ' })
+  return out.split('\n').slice(1,-1).join('\n').replaceAll(/^  /mg,'');
+}
 
 describe('Stop', () => {
   it('renders properly', () => {
@@ -14,7 +22,7 @@ describe('Stop', () => {
           location: Locations.Home,
       }
     } })
-    expect(wrapper).toMatchInlineSnapshot(`
+    expect(clean(wrapper.html())).toMatchInlineSnapshot(`
       <div class="stop_time">2024/10/10 20:00</div>
       <div class="stop_container place">
           <a href="#" class="trip_stop">
