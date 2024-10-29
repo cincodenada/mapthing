6c164bfc45944c29143d407ec32019797ec7f5bf
Ell Bradshaw <ell@c9a.co>
2024-10-28 22:14:21 -0700

    Add some assertions to our import test

--- mapthing/test_uploader.py
+++ mapthing/test_uploader.py
@@ -2,10 +2,16 @@ import pytest
 from pyramid import testing, registry
 
 from mapthing import uploader
-from mapthing.models import getDb, BaseModel, get_session_factory
+from mapthing.models import getDb, BaseModel, get_session_factory, Point
+from mapthing.models import Point, Segment, Track, Source
 
 def test_import(dbengine):
     db = get_session_factory(dbengine)()
     uploader.import_file(db, './fixtures/tiny.gpx')
+
+    assert(db.query(Point).count() == 15)
+    assert(db.query(Segment).count() == 2)
+    assert(db.query(Track).count() == 1)
+    assert(db.query(Source).count() == 1)
 
 
