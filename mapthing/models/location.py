aa362fe6c1211241fb2880df04173606e8be1945
Ell <ell@c9a.co>
2024-11-24 08:19:52 +0000

    Don't fight with the db for autoinc

--- mapthing/models/location.py
+++ mapthing/models/location.py
@@ -26,7 +26,7 @@ class LocationType(str, enum.Enum):
 
 class Location(BaseModel, SerializableMixin):
     __tablename__ = 'locations'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     latitude = Column(Float)
     longitude = Column(Float)
